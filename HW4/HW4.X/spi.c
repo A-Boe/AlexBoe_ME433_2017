8477d70a85b0d980b7db6b1fa74593665e643220
A-Boe <alexjboe@gmail.com>
2017-04-13 18:37:15 -0500

    HW 4 changes

--- HW4/HW4.X/spi.c
+++ HW4/HW4.X/spi.c
@@ -1,132 +1,124 @@
-#include "NU32.h"       // constants, funcs for startup and UART
-// Demonstrates spi by accessing external ram
-// PIC is the master, ram is the slave
-// Uses microchip 23K256 ram chip (see the data sheet for protocol details)
-// SDO4 -> SI (pin F5 -> pin 5)
-// SDI4 -> SO (pin F4 -> pin 2)
-// SCK4 -> SCK (pin B14 -> pin 6)
-// SS4 -> CS (pin B8 -> pin 1)
-// Additional SRAM connections
-// Vss (Pin 4) -> ground
-// Vcc (Pin 8) -> 3.3 V
-// Hold (pin 7) -> 3.3 V (we don't use the hold function)
-// 
-// Only uses the SRAM's sequential mode
-//
-#define CS LATBbits.LATB7       // chip select pin
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include<math.h>         // Math library
+#include<stdio.h>
 
-// send a byte via spi and return the response
+// DEVCFG0
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
+
+// DEVCFG1
+#pragma config FNOSC = FRCPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // use slowest wdt
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
+
+// DEVCFG3
+#pragma config USERID = 00011010 // (26) some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
+
+// Definitions
+#define CLOCK 48000000
+#define CS LATAbits.LATA4       // chip select (CS) pin
+
+void initSPI1(void);
+unsigned char spi_io(unsigned char o);
+void write_dac(unsigned int channel, unsigned int voltage);
+void sineGen(void);
+void sawtGen(void);
+
+static volatile unsigned int sinewave[100];
+static volatile unsigned int sawtwave[200];
+
+int main() {
+
+    __builtin_disable_interrupts();
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+    
+    sineGen();
+    sawtGen();
+    initSPI1();
+    
+    __builtin_enable_interrupts();
+    
+    unsigned int i = 0;
+    unsigned int j = 0;
+    
+    while(1) {
+        _CP0_SET_COUNT(0);
+        while(_CP0_GET_COUNT() < CLOCK/2/1000)  {;}
+        write_dac(1, sinewave[i]);
+        _CP0_SET_COUNT(0);
+        while(_CP0_GET_COUNT() < CLOCK/2/10000)  {;}
+        write_dac(0, sawtwave[j]);
+        i++;
+        j++;
+        if (i == 100)  {
+            i = 0;
+        }
+        if (j == 200)   {
+            j = 0;
+        }
+    }
+}
+
+void initSPI1() {
+    TRISAbits.TRISA4 = 0;     // A4 is CS pin
+    CS = 1;
+    
+    RPA1Rbits.RPA1R = 0b0011; // sets A1 as SDO1 of PIC32
+
+    
+    SPI1CON = 0;              // turn off the spi module and reset it
+    SPI1BUF;                  // clear the rx buffer by reading from it
+    SPI1BRG = 0x3;         // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
+    SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+    SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+    SPI1CONbits.MSTEN = 1;    // master operation
+    SPI1CONbits.ON = 1;       // turn on spi 1
+}
+
+
 unsigned char spi_io(unsigned char o) {
-  SPI1BUF = o;
-  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+    SPI1BUF = o;
+    while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
     ;
-  }
-  return SPI1BUF;
+    }
+    return SPI1BUF;
 }
 
-// initialize spi4 and the ram module
-void ram_init() {
-  // set up the chip select pin as an output
-  // the chip select pin is used by the sram to indicate
-  // when a command is beginning (clear CS to low) and when it
-  // is ending (set CS high)
-  TRISBbits.TRISB7 = 0;
-  CS = 1;
-
-  // Master - SPI4, pins are: SDI4(F4), SDO4(F5), SCK4(F13).  
-  // we manually control SS4 as a digital output (F12)
-  // since the pic is just starting, we know that spi is off. We rely on defaults here
-  TRISAbits.TRISA4 = 0;
-  cs = 1;
-  
-  RPA1Rbits.RPA1R = 0b0011 //example
-  // setup spi4
-  SPI1CON = 0;              // turn off the spi module and reset it
-  SPI1BUF;                  // clear the rx buffer by reading from it
-  SPI1BRG = 0x1000;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
-  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
-  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
-  SPI1CONbits.MSTEN = 1;    // master operation
-  SPI1CONbits.ON = 1;       // turn on spi 4
-  
-                            // send a ram set status command.
-  CS = 0;                   // enable the ram
-  spi_io(0x01);             // ram write status
-  spi_io(0x41);             // sequential mode (mode = 0b01), hold disabled (hold = 0)
-  CS = 1;                   // finish the command
-}
-
-
-// read len bytes from ram, starting at the address addr
-/*void ram_read(unsigned short addr, char data[], int len) {
-  int i = 0;
-  CS = 0;
-  spi_io(0x3);                   // ram read operation
-  spi_io((addr & 0xFF00) >> 8);  // most significant address byte
-  spi_io(addr & 0x00FF);         // least significant address byte
-  for(i = 0; i < len; ++i) {
-    data[i] = spi_io(0);         // read in the data
-  }
-  CS = 1;
-}*/
-
-int main(void) {
-  unsigned short addr1 = 0x1234;                  // the address for writing the ram
-  char data[] = "Help, I'm stuck in the RAM!";    // the test message
-  char read[] = "***************************";    // buffer for reading from ram
-  char buf[100];                                  // buffer for comm. with the user
-  unsigned char status;                           // used to verify we set the status 
-  NU32_Startup();   // cache on, interrupts on, LED/button init, UART init
-  ram_init(); 
-  unsigned int i = 0;
-  
-  unsigned int sinewave[100];
-  unsigned int rampwave[100];
-  double temp;
-  
-  for (i=0; i<100; i++){
-      temp = 255.0/2.0+255/2*sin(2.0*3.14*i/100.0);
-      sinewave[i] = temp;
-      temp = i*255.0/100.0;
-      rampwave[i] = temp;
-  }
-  TRISBbits.TRSIB14 = 0;
-  TRISAbits.TRISBA4 = 0;
-  PORTBbits.RB14 = 1;
-  // check the ram status
-  CS = 0;
-  spi_io(0x5);                                      // ram read status command
-  status = spi_io(0);                               // the actual status
-  CS = 1;
-
-  sprintf(buf, "Status 0x%x\r\n",status);
-  NU32_WriteUART3(buf);
-
-  sprintf(buf,"Writing \"%s\" to ram at address 0x%x\r\n", data, addr1);
-  NU32_WriteUART3(buf);
-                                                    // write the data to the ram
-  ram_write(addr1, data, strlen(data) + 1);         // +1, to send the '\0' character
-  ram_read(addr1, read, strlen(data) + 1);          // read the data back
-  sprintf(buf,"Read \"%s\" from ram at address 0x%x\r\n", read, addr1);
-  NU32_WriteUART3(buf);
-
-  while(1) {
-      _CP0_SET_COUNT(0);
-      while(_CP0_GET_COUNT() < 48000000/2/1000){}
-      write_dac(),sinewave[i]);
-      _CP0_SET_COUNT(0);
-      while (_CP0_SET_COUNT() < 48000000/2/10000){}
-      write_dac(0,rampwave[i]);
-      i++;
-      if(i==100){
-          i=0;
-          
-      }
-  }
-  
-}
-void write_dac(unsigned int channel, insigned int voltage){
-    unsigned char b1=0, b2=0;
+void write_dac(unsigned int channel, unsigned int voltage)  {
+    unsigned char b1 = 0;
+    unsigned char b2 = 0;
     
     b1 = (channel<<7);
     b1 = b1 | (0b1110000);
@@ -134,9 +126,24 @@ void write_dac(unsigned int channel, insigned int voltage){
     
     b2 = voltage<<4;
     
-    cs = 0;
+    CS = 0;
     spi_io(b1);
     spi_io(b2);
-    cs = 1;
+    CS = 1;
+}
+
+void sineGen()  {
+    unsigned int i;
     
+    for (i=0;i<100;i++) {
+        sinewave[i] = 255.0/2.0 + 255.0/2.0*sin(2.0*M_PI*i/100.0);
+    }
+}
+
+void sawtGen()  {
+    unsigned int j;
+    
+    for (j=0;j<200;j++)    {
+        sawtwave[j] = j*225.0/200.0;
+    }
 }
