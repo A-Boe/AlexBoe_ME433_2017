7d9764b37bd04d2243e795651e9586f0c145f936
A-Boe <alexjboe@gmail.com>
2017-04-13 16:48:27 -0500

    HW 4 code

---
+++ HW4/HW4.X/spi.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   spi.h
+ * Author: Alex
+ *
+ * Created on April 10, 2017, 6:39 PM
+ */
+
+#ifndef SPI_H
+#define	SPI_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SPI_H */
+
