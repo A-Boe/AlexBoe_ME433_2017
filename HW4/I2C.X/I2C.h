2e8f48e7af2e4f0f250240e5855b05d3c3d6bda8
A-Boe <alexjboe@gmail.com>
2017-04-17 14:25:14 -0500

    Header file

---
+++ HW4/I2C.X/I2C.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   I2C.h
+ * Author: Alex
+ *
+ * Created on April 17, 2017, 2:17 PM
+ */
+
+#ifndef I2C_H
+#define	I2C_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* I2C_H */
+
