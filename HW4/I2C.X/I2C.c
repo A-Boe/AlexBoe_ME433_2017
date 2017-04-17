be9efc57a8efc3df784decaf85c218b6644af7c2
A-Boe <alexjboe@gmail.com>
2017-04-17 14:25:07 -0500

    Main file

---
+++ HW4/I2C.X/I2C.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   I2C.c
+ * Author: Alex
+ *
+ * Created on April 17, 2017, 2:08 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
