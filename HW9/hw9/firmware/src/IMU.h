27197d67a9ee759fe92bcf60993905c5c1bedd0b
A-Boe <alexjboe@gmail.com>
2017-06-04 16:25:58 -0500

    hw 9 init

---
+++ HW9/hw9/firmware/src/IMU.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   IMU.h
+ * Author: Alex
+ *
+ * Created on June 4, 2017, 4:15 PM
+ */
+
+#ifndef IMU_H
+#define	IMU_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* IMU_H */
+
---
+++ HW9/hw9/firmware/src/i2c.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   i2c.h
+ * Author: Alex
+ *
+ * Created on June 4, 2017, 4:18 PM
+ */
+
+#ifndef I2C_H
+#define	I2C_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* I2C_H */
+
